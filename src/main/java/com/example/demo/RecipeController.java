c7f77ee4184fba5c24972c845866d7af30a7c70b
lisatran03 <lisa.tran@student.htw-berlin.de>
2025-11-08 16:30:21 +0100

    angepasste Controller mit CORS

--- src/main/java/com/example/demo/RecipeController.java
+++ src/main/java/com/example/demo/RecipeController.java
@@ -1,9 +1,12 @@
 package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://prog3-frontend-fhkb.onrender.com")
 public class RecipeController {
 
         @GetMapping("/recipes")
