08d78c9198eb365084da28531cbead6d8116aac1
lisatran03 <lisa.tran@student.htw-berlin.de>
2025-11-11 15:30:25 +0100

    CORS

--- src/main/java/com/example/demo/RecipeController.java
+++ src/main/java/com/example/demo/RecipeController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://prog3-frontend-fhkb.onrender.com")
 public class RecipeController {
 
-        @GetMapping("/recipes")
-        public List<Recipe> getRecipe() {
+    @CrossOrigin(origins = {
+            "https://prog3-frontend-fhkb.onrender.com",
+            "http://localhost:5173"
+    })
+    @GetMapping("/recipes")
+    public List<Recipe> getRecipe() {
             return List.of(new Recipe("Oatmeal", "Breakfast", "Oats, Milk, Honey", "Cook oats with milk and add honey."),
                            new Recipe("Grilled Cheese Sandwich", "Lunch", "Bread, Cheese, Butter", "Butter bread, add cheese, and grill."),
                            new Recipe("Spaghetti Bolognese", "Dinner", "Spaghetti, Ground Beef, Tomato Sauce", "Cook spaghetti and prepare sauce with ground beef."));
